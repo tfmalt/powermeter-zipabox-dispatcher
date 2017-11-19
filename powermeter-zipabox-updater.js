aec9ededa4f61dcc88b88ce74a49865d7d04797c
Thomas Malt <thomas.malt@knowit.no>
2017-11-19 17:37:54 +0100

    added hook for cumulative power usage

--- powermeter-zipabox-updater.js
+++ powermeter-zipabox-updater.js
@@ -27,24 +27,45 @@ log.add(log.transports.Console, {
     silent:      false,
     timestamp:   true
 });
+
 log.info("Starting PowerMeter Zipabox Dispatcher v" + pkg.version);
 log.debug("Log level debug");
 log.debug("These are the options:", opts);
 
 const cron = new CronEmitter();
 cron.add('*/30 * * * * *', 'every_thirty_seconds');
-cron.add('30 0 * * * *', 'every_hour');
+cron.add('25 0 * * * *', 'every_hour');
+cron.add('20 */5 * * * *', 'every_five_minutes');
+
+cron.on('every_five_minutes', (name) => {
+    log.debug("got event: every_five_minutes");
+    fetch(`https://api.malt.no/power/kwh/today?apikey=${opts.malt.apikey}`)
+        .then(res => {
+            log.debug("kwh today HTTP status")
+            return res.json();
+        })
+        .then(data => {
+            log.info("Got valid response from power meter api: today kwh =", data.kwh);
+            return fetch(`https://my.zipato.com/zipato-web/remoting/attribute/set?serial=${opts.zipabox.serial}&apiKey=${opts.zipabox.apikey}&ep=${opts.zipabox.device}&value4=${data.kwh}`);
+        })
+        .then(res => {
+            log.info("zipato said status for kwh today was ", res.status);
+        })
+        .catch(err => {
+            log.error("exception:", err.message);
+        })
+});
 
 cron.on('every_hour', (name) => {
     log.debug("Got event: every_hour");
 
     fetch(`https://api.malt.no/power/kwh/hour/1?apikey=${opts.malt.apikey}`)
         .then(res => {
-            log.debug("HTTP Status: ", res.status);
+            log.debug("kwh hour HTTP Status: ", res.status);
             return res.json();
         })
         .then(data => {
-            log.info("Got valid response from power meter api: kwh = ", data.total);
+            log.info("Got valid response from power meter api: kwh =" , data.total);
             return fetch(
                 `https://my.zipato.com/zipato-web/remoting/attribute/set?serial=${opts.zipabox.serial}&apiKey=${opts.zipabox.apikey}&ep=${opts.zipabox.device}&value3=${data.total}`
             );
@@ -62,7 +83,7 @@ cron.on('every_thirty_seconds', (name) => {
 
     fetch(`https://api.malt.no/power/watts/30?apikey=${opts.malt.apikey}`)
         .then( res => {
-            log.debug("HTTP Status: ", res.status);
+            log.debug("watts HTTP Status: ", res.status);
             if (res.status !== 200) {
                 throw new ReferenceError("http status: " + res.status);
             }
