e9e405a2804f7711c410383bfe7e42ff71fd0043
Thomas Malt <thomas.malt@knowit.no>
2017-11-19 15:33:22 +0100

    Added configuration paramters

--- powermeter-zipabox-updater.js
+++ powermeter-zipabox-updater.js
@@ -3,22 +3,33 @@
  * powermeter-zipabox-updater
  *  
  */
-const fetch = require("node-fetch");
-const log = require("winston");
-const pkg = require("./package.json");
+const fetch       = require("node-fetch");
+const log         = require("winston");
+const pkg         = require("./package.json");
 const CronEmitter = require("cron-emitter");
 
+const opts = {
+    malt: {
+        apikey: process.env.POWERMETER_APIKEY,
+    },
+    zipabox: {
+        apikey: process.env.ZIPATO_APIKEY,
+        serial: process.env.ZIPABOX_SERIAL,
+        device: process.env.ZIPABOX_DEVICE
+    }
+}
 
 log.remove(log.transports.Console);
 log.add(log.transports.Console, {
-    level:       (process.env.POWERMETER_ZIPATO_DEBUG === 1) ? "debug" : "info",
+    level:       (process.env.POWERMETER_ZIPATO_DEBUG == 1) ? "debug" : "info",
     prettyPrint: true,
     colorize:    false,
     silent:      false,
     timestamp:   true
 });
 log.info("Starting PowerMeter Zipabox Dispatcher v" + pkg.version);
-log.info("  Log level debug");
+log.debug("  Log level debug");
+log.debug("  These are the options:", opts);
 
 const cron = new CronEmitter();
 cron.add('*/30 * * * * *', 'every_thirty_seconds');
@@ -27,7 +38,7 @@ cron.add('30 0 * * * *', 'every_hour');
 cron.on('every_hour', (name) => {
     log.debug("Got event: every_hour");
 
-    fetch("https://api.malt.no/power/kwh/hour/1?apikey=9995565173874666965f83319c0f9497")
+    fetch(`https://api.malt.no/power/kwh/hour/1?apikey=${opts.malt.apikey}`)
         .then(res => {
             log.debug("HTTP Status: ", res.status);
             return res.json();
@@ -35,7 +46,7 @@ cron.on('every_hour', (name) => {
         .then(data => {
             log.info("Got valid response from power meter api: kwh = ", data.total);
             return fetch(
-                `https://my.zipato.com/zipato-web/remoting/attribute/set?serial=0124671A0E01C58A&apiKey=42562d52-4d46-4a43-9010-40a576dfc4e2&ep=fa91af9f-548b-41cc-8249-bf9654391ea0&value3=${data.total}`
+                `https://my.zipato.com/zipato-web/remoting/attribute/set?serial=${opts.zipabox.serial}&apiKey=${opts.zipabox.apikey}&ep=${opts.zipabox.device}&value3=${data.total}`
             );
         })
         .then(res => {
@@ -49,7 +60,7 @@ cron.on('every_hour', (name) => {
 cron.on('every_thirty_seconds', (name) => {
     log.debug("Got event: every_thirty_seconds");
 
-    fetch("https://api.malt.no/power/watts/30?apikey=9995565173874666965f83319c0f9497")
+    fetch(`https://api.malt.no/power/watts/30?apikey=${opts.malt.apikey}`)
         .then( res => {
             log.debug("HTTP Status: ", res.status);
             if (res.status !== 200) {
@@ -60,7 +71,7 @@ cron.on('every_thirty_seconds', (name) => {
         .then(data => {
             log.info("Got valid response from power meter api: watt = ", data.watt);
             return fetch(
-                `https://my.zipato.com/zipato-web/remoting/attribute/set?serial=0124671A0E01C58A&apiKey=42562d52-4d46-4a43-9010-40a576dfc4e2&ep=fa91af9f-548b-41cc-8249-bf9654391ea0&value2=${data.watt}`
+                `https://my.zipato.com/zipato-web/remoting/attribute/set?serial=${opts.zipabox.serial}&apiKey=${opts.zipabox.apikey}&ep=${opts.zipabox.device}&value2=${data.watt}`
             );
         })
         .then(res => {
